2a6e641c7992db0ad2f3469c49e71fb886c2bd64
Luis Vinicius <viniborges@yahoo.com.br>
2024-07-07 15:13:13 -0300

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import compress from 'astro-compress'
+import icon from 'astro-icon'
+
+// https://astro.build/config
+export default defineConfig({
+  compressHTML: false,
+  integrations: [
+    mdx(),
+    icon(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+  ],
+})
