bf1481585682b1d6f078af353f31c6688aac183a
Luis Vinicius <viniborges@yahoo.com.br>
2024-09-19 23:36:04 -0300

    add sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,16 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
-import icon from 'astro-icon'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import icon from 'astro-icon';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://viniborges.com',
   compressHTML: true,
-  integrations: [
-    mdx(),
-    icon(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    compress(),
-  ],
-})
+  integrations: [mdx(), icon(), tailwind({
+    applyBaseStyles: false
+  }), compress(), sitemap()]
+});
